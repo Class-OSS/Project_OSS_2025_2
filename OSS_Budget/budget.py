7d5858cd53896aedc0c640b1337e34017ea7b6af
Heegwon Ahn <ahg3619@naver.com>
2025-11-25 10:22:22 +0900

    [기능추가] 수입 추가 기능, 수입 목록 보기 기능, 총 지출에서 총 금액으로 변경

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -4,12 +4,21 @@ from expense import Expense
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.incomes = []
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
         expense = Expense(today, category, description, amount)
         self.expenses.append(expense)
         print("지출이 추가되었습니다.\n")
+    
+    # 가계부 수입 기능 추가
+    
+    def add_income(self, category, description, amount):
+        today = datetime.date.today().isoformat()
+        income = Expense(today, category, description, amount)
+        self.incomes.append(income)
+        print("수입이 추가되었습니다.\n")
 
     def list_expenses(self):
         if not self.expenses:
@@ -19,9 +28,23 @@ class Budget:
         for idx, e in enumerate(self.expenses, 1):
             print(f"{idx}. {e}")
         print()
-
-    def total_spent(self):
-        total = sum(e.amount for e in self.expenses)
-        print(f"총 지출: {total}원\n")
+    
+    # 가계부 수입 목록 기능 추가
+    
+    def list_incomes(self):
+        if not self.incomes:
+            print("수입 내역이 없습니다.\n")
+            return
+        print("\n[수입 목록]")
+        for idx, e in enumerate(self.incomes, 1):
+            print(f"{idx}. {e}")
+        print()
+    
+    # 수입 기능 추가에 따른 총 지출에서 총 금액으로 변경
+    
+    def total_money(self):
+        total -= sum(e.amount for e in self.expenses)
+        total = sum(e.amount for e in self.incomes)
+        print(f"총 금액: {total}원\n")
 
 
