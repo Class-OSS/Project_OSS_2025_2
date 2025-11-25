4257b7d3f11607f45df1161bac67ec34a6f26bce
Kang <bear36031@yu.ac.kr>
2025-11-25 23:10:29 +0900

    [기능추가] 계산기에 거듭제곱과 e, pi를 사용할 수 있도록 함.

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import math # e와 π의 값을 추가하기 위함 
 
 
 class Calculator:
@@ -13,13 +14,13 @@ class Calculator:
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
 
-        # 버튼 생성
+        # 버튼 생성, e, π, ^(거듭제곱) 기호를 추가.
         buttons = [
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['e', 'π','^', '=']
         ]
 
         for row in buttons:
@@ -39,9 +40,18 @@ class Calculator:
             self.expression = ""
         elif char == '=':
             try:
+                self.expression = self.expression.replace('e', str(math.e))# e를 정확한 값으로 변경
+                self.expression = self.expression.replace('π', str(math.pi))# π를 정확한 값으로 변경
+                self.expression = self.expression.replace('^', '**')# 거듭제곱 기호를 **로 치환
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
+        elif char == '^': # 거듭제곱 기호가 눌리면
+            self.expression += '^'
+        elif char == 'e': # e가 눌리면 e를 수식에 추가 
+            self.expression += 'e'
+        elif char == 'π': # π가 눌리면 π를 수식에 추가 
+            self.expression += 'π'
         else:
             self.expression += str(char)
 
