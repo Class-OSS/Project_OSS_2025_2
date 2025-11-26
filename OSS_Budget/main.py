0500c946e08a7ce869987be398e9d3fcbca1984b
Kang <bear36031@yu.ac.kr>
2025-11-26 21:10:30 +0900

    [기능추가] 가계부에서 수입 추가, 수입 목록 확인, 수입과 지출의 비율 출력 기능 추가.

--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -1,18 +1,21 @@
-from budget import Budget
+from budget import Budget, Budget_income
 
 
 def main():
     budget = Budget()
-
+    budget_in = Budget_income()
+    
     while True:
         print("==== 간단 가계부 ====")
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
-        print("3. 총 지출 보기")
-        print("4. 종료")
+        print("3. 수입 추가") # 기능 추가
+        print("4. 수입 목록 보기") # 기능 추가
+        print("5. 총 수입 / 총 지출 보기 / 비율 보기") # 기능 추가
+        print("6. 종료")
         choice = input("선택 > ")
 
-        if choice == "1":
+        if choice == "1": # 지출 추가
             category = input("카테고리 (예: 식비, 교통 등): ")
             description = input("설명: ")
             try:
@@ -22,13 +25,26 @@ def main():
                 continue
             budget.add_expense(category, description, amount)
 
-        elif choice == "2":
+        elif choice == "2": # 지출 목록 보기
             budget.list_expenses()
-
-        elif choice == "3":
-            budget.total_spent()
-
-        elif choice == "4":
+            
+        elif choice == "3": # 수입 추가
+            category = input("카테고리 (예: 용돈, 알바, 배당금 등): ")
+            description = input("설명: ")
+            try:
+                amount = int(input("금액(원): "))
+            except ValueError:
+                print("잘못된 금액입니다.\n")
+                continue
+            budget_in.add_incomes(category, description, amount)
+            
+        elif choice == "4": # 수입 목록 보기
+            budget_in.list_incomes()
+            
+        elif choice == "5": # 총 수입, 총 지출, 비율 한번에 보여주기
+            budget.percentage(budget_in)
+            
+        elif choice == "6":
             print("가계부를 종료합니다.")
             break
 
