533b826456c8d662b08b653fac7ac675480717d2
MinChaeChoo <alsco0549@gamil.com>
2025-11-26 19:35:45 +0900

    [기능추가] 지출내역 최대/최소 항목 출력 기능 추가

--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -9,7 +9,8 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 최대/최소 지출 보기")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -29,6 +30,11 @@ def main():
             budget.total_spent()
 
         elif choice == "4":
+            print("\n 최대/최소 지출 ")
+            budget.max_expense()
+            budget.min_expense()
+
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
