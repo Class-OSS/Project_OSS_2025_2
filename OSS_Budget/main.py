5c0b62e0d3f3b8ae2c846bbc9ce30ca55ebed80c
WooJung Choi <woojung2022@yu.ac.kr>
2025-11-26 13:43:48 +0900

    가계부 지출 내역 수정 및 삭제 기능 추가 26/11/2025

--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -9,7 +9,9 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 지출 수정") ##지출 수정&삭제 목록 추가
+        print("5. 지출 삭제")
+        print("6. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -28,7 +30,26 @@ def main():
         elif choice == "3":
             budget.total_spent()
 
-        elif choice == "4":
+        elif choice == "4": ## 지출 수정
+            try:
+                idx = int(input("수정할 항목 번호: ")) -1
+                n_category = input("카테고리: ")
+                n_description = input("설명: ")
+                n_amount = int(input("금액(원): "))
+            except ValueError:
+                print("잘못된 입력입니다.\n")
+                continue
+            budget.edit_expense(idx, n_category, n_description, n_amount)
+
+        elif choice == "5": ## 지출 삭제
+            try:
+                idx = int(input("삭제할 항목 번호: ")) - 1
+            except ValueError:
+                print("잘못된 입력입니다.\n")
+                continue
+            budget.delete_expense(idx)
+
+        elif choice == "6":
             print("가계부를 종료합니다.")
             break
 
