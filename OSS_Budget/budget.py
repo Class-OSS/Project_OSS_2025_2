baa00bb64584062831e0f50be3016958b97925a1
jjaazzzz <chlwnstj0430@naver.com>
2025-11-27 20:02:12 +0900

    [기능추가] 지출을 삭제 또는 수정할 수 있는 함수 추가(main도 함께 수정)

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -5,7 +5,7 @@ class Budget:
     def __init__(self):
         self.expenses = []
 
-    def add_expense(self, category, description, amount):
+    def add_expense(self, category , description, amount):
         today = datetime.date.today().isoformat()
         expense = Expense(today, category, description, amount)
         self.expenses.append(expense)
@@ -24,4 +24,21 @@ class Budget:
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
 
+    # 지출 삭제 기능 추가
+    def delete_expense(self, index):
+        if (1 <= index <= len(self.expenses)):
+            del self.expenses[index - 1]
+            print(f"{index}번 지출이 삭제되었습니다.\n")
+        else:
+            print("잘못된 번호입니다.\n")
 
+    # 지출 수정 기능 추가
+    def edit_expense(self, index, new_category, new_description, new_amount):
+        if (1 <= index <= len(self.expenses)):
+            expense = self.expenses[index - 1]
+            expense.category = new_category
+            expense.description = new_description
+            expense.amount = new_amount
+            print(f"{index}번 지출이 수정되었습니다 => {expense}\n")
+        else:
+            print("잘못된 번호입니다.\n")
