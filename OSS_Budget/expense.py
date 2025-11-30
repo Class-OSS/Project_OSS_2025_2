a4e322363a34acec7b72641df60b162f7d05f77d
SOHYEON MOON <04veronica@naver.com>
2025-11-30 14:52:48 +0900

    [기능추가] 사용자 입력에 따른 적금 총 금액 계산

--- OSS_Budget/expense.py
+++ OSS_Budget/expense.py
@@ -8,3 +8,8 @@ class Expense:
 
     def __str__(self):
         return f"[{self.date}] {self.category} - {self.description}: {self.amount}원"
+    #적금 기능 : 사용자 입력에 따른 적금 계산
+    def saving(self,monthly_amount ,months) :
+        total_saving = monthly_amount * months
+        return total_saving
+    
