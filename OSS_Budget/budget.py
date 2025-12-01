c1218faef0e91cd444f71b13af5c46225121b700
Kim DongWoo <1221kdw@yu.ac.kr>
2025-12-01 12:04:40 +0900

    [기능 추가] txt파일 생성 후 지출 내역 저장 및 불러오기 기능

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -4,14 +4,18 @@ from expense import Expense
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.load_from_txt()
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
         expense = Expense(today, category, description, amount)
         self.expenses.append(expense)
-        print("지출이 추가되었습니다.\n")
+        print("지출이 추가되었습니다.")
+        self.save_to_txt()
 
     def list_expenses(self):
+        self.load_from_txt()
+        
         if not self.expenses:
             print("지출 내역이 없습니다.\n")
             return
@@ -23,5 +27,22 @@ class Budget:
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
+
+    def save_to_txt(self):
+        with open("expenses.txt", "w", encoding="utf-8") as f:
+            for exp in self.expenses:
+                f.write(f"{exp.date},{exp.category},{exp.description},{exp.amount}\n")
+        print("expenses.txt에 저장되었습니다.\n")
+
+    def load_from_txt(self):
+        self.expenses = []
+        
+        try:
+            with open("expenses.txt", "r", encoding="utf-8") as f:
+                for line in f:
+                    date, category, description, amount = line.strip().split(",")
+                    self.expenses.append(Expense(date, category, description, int(amount)))
+        except FileNotFoundError:
+            pass
 
 
