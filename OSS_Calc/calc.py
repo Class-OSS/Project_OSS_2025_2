1d24edcb7ad78caa35b760ecca01d75b004617c6
choi jaewon <22313579@yu.ac.kr>
2025-12-01 16:02:59 +0900

    calc.py [기능추가] 공학용(제곱, 제곱근) 계산 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,31 +1,30 @@
 import tkinter as tk
-
+import math
 
 class Calculator:
     def __init__(self, root):
         self.root = root
-        self.root.title("계산기")
-        self.root.geometry("300x400")
+        self.root.title("공학용 계산기") 
+        self.root.geometry("300x450") 
 
         self.expression = ""
-
-        # 입력창
+    
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
 
-        # 버튼 생성
         buttons = [
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['x²', '√', '=']      # 
         ]
 
         for row in buttons:
             frame = tk.Frame(root)
             frame.pack(expand=True, fill="both")
             for char in row:
+                
                 btn = tk.Button(
                     frame,
                     text=char,
@@ -35,18 +34,29 @@ class Calculator:
                 btn.pack(side="left", expand=True, fill="both")
 
     def on_click(self, char):
-        if char == 'C':
-            self.expression = ""
-        elif char == '=':
-            try:
+        try:
+            if char == 'C':
+                self.expression = ""
+            
+            elif char == '=':
                 self.expression = str(eval(self.expression))
-            except Exception:
-                self.expression = "에러"
-        else:
-            self.expression += str(char)
+            
+            elif char == 'x²':
+                val = eval(self.expression)
+                self.expression = str(val ** 2)
+            
+            elif char == '√':
+                val = eval(self.expression)
+                self.expression = str(math.sqrt(val))
+            
+            else:
+                self.expression += str(char)
+            
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, self.expression)
 
-        self.entry.delete(0, tk.END)
-        self.entry.insert(tk.END, self.expression)
-
-
-
+        except Exception:
+            self.expression = "에러"
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, self.expression)
+            self.expression = ""
