d81d4e87e6f4746875967d40e088b9c07521a1da
WooJung Choi <woojung2022@yu.ac.kr>
2025-12-01 15:22:53 +0900

    [22212088 최우중] 계산기 키보드 입력 및 괄호 기능 추가 01/12/2025

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -18,9 +18,16 @@ class Calculator:
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
-            ['0', '.', 'C', '+'],
-            ['=']
+            ['0', '.', 'C[Esc]', '+'],
+            ['(', ')', 'Ans[A]', '='] #괄호, Ans 버튼 추가
         ]
+
+        # Enter 키를 on_enter 함수에 연결
+        self.entry.bind("<Return>", self.on_enter)
+        self.root.bind("<Return>", self.on_enter)
+
+        # 버튼을 키보드에 할당
+        self.root.bind("<Key>", self.on_key)
 
         for row in buttons:
             frame = tk.Frame(root)
@@ -39,14 +46,58 @@ class Calculator:
             self.expression = ""
         elif char == '=':
             try:
-                self.expression = str(eval(self.expression))
+                result = str(eval(self.expression))
+                self.expression = result
+                self.last_result = result  # 마지막 값 저장
             except Exception:
                 self.expression = "에러"
+
+        # 마지막 결과를 현 수식 뒤에 붙이기
+        elif char == 'Ans':
+            if self.last_result is not None:
+                self.expression += str(self.last_result)
+
         else:
             self.expression += str(char)
 
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
 
+    def on_enter(self, event=None):
+        # Enter 키를 누르면 바로 수식 계산 ('='과 동일한 기능)
+        self.on_click('=')
 
+    def on_key(self, event):
+        key = event.keysym
+        char = event.char
+        
+        # 숫자
+        if char.isdigit():
+            self.on_click(char)
 
+        # 연산자
+        elif char in ['+', '-', '*', '/', '.']:
+            self.on_click(char)
+
+        elif key == "BackSpace":
+            self.expression = self.expression[:-1]
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, self.expression)   
+
+        # Esc -> 전체 초기화 (C)
+        elif key == "Escape":
+            self.on_click('C')
+
+        # 괄호
+        elif char == '(':
+            self.on_click('(')
+        elif char == ')':
+            self.on_click(')')
+
+        # A  -> Ans
+        elif key.lower() == 'a':
+            self.on_click('Ans')
+
+        # = 버튼
+        elif char == '=':
+            self.on_click('=')
