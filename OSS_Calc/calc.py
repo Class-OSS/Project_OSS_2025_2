a58cd7a67acc8dc477a16b484bddeda4061b4da4
JaeYeong Kim <kjeayoung@yu.ac.kr>
2025-12-02 21:22:23 +0900

    [기능추가] 계산기 공학용 연산(제곱, 제곱근) 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import math
 
 class Calculator:
     def __init__(self, root):
@@ -19,7 +19,7 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['sqrt', '^2', '=']   # 기능 추가된 부분
         ]
 
         for row in buttons:
@@ -37,6 +37,27 @@ class Calculator:
     def on_click(self, char):
         if char == 'C':
             self.expression = ""
+            
+        elif char == 'sqrt':
+            try:
+                # 식이 비어있지 않을 때만 계산
+                if self.expression:
+                    val = eval(self.expression)
+                    if val < 0:
+                        self.expression = "에러" # 음수 제곱근 방지
+                    else:
+                        self.expression = str(math.sqrt(val))
+            except:
+                self.expression = "에러"
+
+        elif char == '^2':
+            try:
+                if self.expression:
+                    # 입력된 식을 괄호로 묶고 제곱
+                    self.expression = str(eval(self.expression) ** 2)
+            except:
+                self.expression = "에러"
+
         elif char == '=':
             try:
                 self.expression = str(eval(self.expression))
@@ -47,6 +68,3 @@ class Calculator:
 
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
-
-
-
