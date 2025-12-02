97e6946ffc7d6a39f6e7f50917254956f89ba6ed
Leeharim <harim826@naver.com>
2025-12-02 20:47:10 +0900

    [기능추가] 임시 비밀번호 생성 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import simpledialog
+import random  # 랜덤 기능
+import string  # 글자 조합 기능
 
 class Calculator:
     def __init__(self, root):
@@ -9,17 +10,17 @@ class Calculator:
 
         self.expression = ""
 
-        # 입력창
+        
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
 
-        # 버튼 생성 (1/N 버튼 포함)
+       
         buttons = [
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=', '1/N']
+            ['=', 'PW'] 
         ]
 
         for row in buttons:
@@ -39,33 +40,29 @@ class Calculator:
             self.expression = ""
         elif char == '=':
             try:
-               
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
-        elif char == '1/N':
-            # 더치페이 기능
+        
+        # 비밀번호 생성 기능 
+        elif char == 'PW':
             try:
-                total = simpledialog.askinteger("더치페이", "총 금액을 입력하세요")
-                if total: # 금액을 입력 했다면
-                    people = simpledialog.askinteger("더치페이", "인원 수를 입력하세요")
-                    if people and people > 0:
-                        result = total // people
-                        self.expression = str(result)
-                    else:
-                        self.expression = "인원 오류"
+               
+                src = string.ascii_letters + string.digits
+                # 8자리 랜덤 생성
+                pw = ''.join(random.sample(src, 8))
+                self.expression = pw
             except:
                 self.expression = "오류"
+        
         else:
             self.expression += str(char)
         
-        # 화면 갱신
+       
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
-
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = Calculator(root)
     root.mainloop()
-        
