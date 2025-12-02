7ad1c174374850859b4f852bccad7031b0fa003c
XIE BINGMAN <bingmanxie@gmail.com>
2025-12-02 18:00:46 +0900

    [기능추가] 금액 유효성 검사 기능 추가

--- OSS_Budget/expense.py
+++ OSS_Budget/expense.py
@@ -4,6 +4,10 @@ class Expense:
         self.date = date
         self.category = category
         self.description = description
+        if not isinstance(amount,(int,float)):
+           raise ValueError("금액은 숫자 타입이어야 합니다")
+        if amount<=0:
+           raise ValueError("지출 금액은 음수 또는 0일 수 없습니다")
         self.amount = amount
 
     def __str__(self):
