55240df664654407b4dd25bf9b9672624e9efcda
DESKTOP-JM6O1IG\정현준 <hjjjung1@yu.ac.kr>
2025-12-03 23:02:08 +0900

    [기능추가] 계산기 백스페이스(←) 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -6,6 +6,10 @@ def on_click(self, char):
         # ^ 입력 시 파이썬 제곱 연산자 ** 를 넣어준다
         self.expression += "**"
 
+    elif char == '←':
+        # 마지막 한 글자 삭제
+        self.expression = self.expression[:-1]
+
     elif char == '=':
         try:
             self.expression = str(eval(self.expression))
@@ -17,4 +21,3 @@ def on_click(self, char):
 
     self.entry.delete(0, tk.END)
     self.entry.insert(tk.END, self.expression)
-
