4088ea4d927fedf21ecd232805fa3b312d76bc6b
HaYoung Jang <jhy45701482@gmail.com>
2025-12-03 18:26:19 +0900

    [기능추가] 지출 분석 결과 추가

--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -12,8 +12,9 @@ def main():
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
         print("4. CSV로 내보내기")
-        print("5. 종료하기")
-        choice = input("선택 > ")
+        print("5. 지출 분석 결과")
+        print("6. 종료하기")
+        choice = input("원하는 기능 선택하세요 -> ")
 
         if choice == "1":
             category = input("카테고리 (예: 식비, 교통 등): ")
@@ -36,6 +37,9 @@ def main():
             print("CSV 저장 완료:", result, "\n")
 
         elif choice == "5":
+            budget.analyze_expenses()
+
+        elif choice == "6":
             print("가계부를 종료합니다.")
             break
 
