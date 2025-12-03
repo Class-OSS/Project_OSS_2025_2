de554dc9b3d84728a4b8034417b4e9b480416565
gasirad <fhdlrkt6973@gmail.com>
2025-12-04 00:04:19 +0900

    [기능추가] 2진수 변환기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -19,7 +19,7 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['BIN', '=']
         ]
 
         for row in buttons:
@@ -42,6 +42,12 @@ class Calculator:
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
+        elif char == 'BIN':
+            try:
+                value = int(eval(self.expression)) 
+                self.expression = bin(value)        
+            except Exception:
+                self.expression = "오류"
         else:
             self.expression += str(char)
 
