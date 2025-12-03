4cd55dc329334c40762118bbe88f298b6334c11a
DESKTOP-JM6O1IG\정현준 <hjjjung1@yu.ac.kr>
2025-12-03 23:10:40 +0900

    [기능추가] 계산기 제곱근(√) 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,14 +1,19 @@
 def on_click(self, char):
     if char == 'C':
+        # 전체 지우기
         self.expression = ""
 
-    elif char == '^':
-        # ^ 입력 시 파이썬 제곱 연산자 ** 를 넣어준다
-        self.expression += "**"
-
-    elif char == '←':
-        # 마지막 한 글자 삭제
-        self.expression = self.expression[:-1]
+    elif char == '√':
+        # 현재 식을 평가해서 제곱근 계산
+        try:
+            if self.expression != "":
+                value = eval(self.expression)
+                self.expression = str(math.sqrt(value))
+            else:
+                # 아무것도 없을 때 √ 누르면 0으로 처리
+                self.expression = "0"
+        except Exception:
+            self.expression = "에러"
 
     elif char == '=':
         try:
@@ -17,7 +22,9 @@ def on_click(self, char):
             self.expression = "에러"
 
     else:
+        # 숫자/연산자 입력 처리
         self.expression += str(char)
 
+    # 화면에 표시 갱신
     self.entry.delete(0, tk.END)
     self.entry.insert(tk.END, self.expression)
